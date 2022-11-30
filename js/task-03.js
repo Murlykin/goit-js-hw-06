0c4dfe5a9a357ca772b4ded575dfbddc5de1cc81
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2022-11-30 09:14:01 +0200

    3

--- js/task-03.js
+++ js/task-03.js
@@ -12,19 +12,19 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
-const list = document.querySelector('.gallery')
+const makeImagesGallery = image => {
+  const { url, alt } = image;
 
-const markup = images
-	.map(image => `<li><img class="picture" src=${image.url} width = '400' alt='${image.alt}'></img></li>`)
-	.join('')
+  return `
+  <li class="gallery__item">
+    <img class="gallery__img" src = ${url} alt = ${alt} width="700"
+  </li>
+  `;
+};
 
-  list.insertAdjacentHTML('beforeend', markup)
-    // const img = document.createElement('img')
-  
-const pictures = document.querySelectorAll('.picture')
+const galleryRef = document.querySelector('.gallery');
 
-list.style.display = 'flex';
-list.style.flexWrap = 'column-reverse'
-list.style.listStyle = 'none'
-list.style.gap = '20px'
-list.style.justifyContent = 'center'
+const makeGallerys = images.map(makeImagesGallery).join('');
+console.log(makeGallerys);
+
+galleryRef.insertAdjacentHTML('afterbegin', makeGallerys);
